e016d48e6c836d76d996cf7edffb7cc43ce3607b
Asemokamichi <asemokamichi@gmail.com>
2023-04-06 11:31:20 +0600

    SQL Migrations

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/Asemokamichi/greenlight/internal/data"
 	_ "github.com/lib/pq"
 )
 
@@ -29,6 +30,7 @@ type config struct {
 type application struct {
 	config config
 	logger *log.Logger
+	models data.Models
 }
 
 func main() {
@@ -53,10 +55,12 @@ func main() {
 	}
 
 	defer db.Close()
+	logger.Printf("database connection pool established")
 
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	srv := &http.Server{
