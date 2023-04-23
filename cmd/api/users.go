dbc0fbceb3a11b635f4192c90989f699492da468
Asemokamichi <asemokamichi@gmail.com>
2023-04-23 17:34:30 +0600

    f

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -47,6 +47,11 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	// if err := app.mailer.Send(user.Email, "user_welcome.tmpl", user); err != nil {
+	// 	app.serverErrorResponse(w, r, err)
+	// 	return
+	// }
+
 	if err := app.writeJSON(w, http.StatusOK, envelope{"user": user}, nil); err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
